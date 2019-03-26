7d75a756a3d17ee1825ff8ceaba1e88bb47a6402
OhwMaI <prasit1120@gmail.com>
2019-03-26 09:25:57 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'VQZWdW9PdMuQ5GpIODDXAjhXZT7LUzhmuSv3qvwX3GrspKHFtyNamZg1GMeW7qptvZVRG7i6qawnnwZFWy6r/+W1W0GGGeLV5bZABc2+o924sNhroWFimh1DIvLEqPRMK13cSYHlGEkxax0H7O8LPgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
